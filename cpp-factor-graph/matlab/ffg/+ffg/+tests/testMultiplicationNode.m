0335c1b29d5086e2dd6223221f8023db0f9e63e7
Timur Bagautdinov <psycharo@gmail.com>
2012-12-06 17:03:00 +0100

    Closed #34,#27 c++: fixed tests, added dev doc

--- cpp-factor-graph/matlab/ffg/+ffg/+tests/testMultiplicationNode.m
+++ cpp-factor-graph/matlab/ffg/+ffg/+tests/testMultiplicationNode.m
@@ -3,28 +3,29 @@ initTestSuite;
 
 function testForward
 
-load('+ffg/+tests/data_gaussian');
+DIM = 3;
 
 nwk = ffg.Network;
 x = ffg.EvidenceNode;
 y = ffg.EvidenceNode;
 
-MATRIX = TestMultiplication.INPUT1.matrix;
+MATRIX = randn(DIM, DIM);
 A = ffg.MultiplicationNode;
 A.setMatrix(MATRIX);
 
 nwk.addEdge(x, A);
 nwk.addEdge(A, y);
 
-% msg = 
-INPUT_MSG = struct('type', 'VARIANCE', 'mean', TestMultiplication.INPUT1.mean, 'var', TestMultiplication.INPUT1.var);
+% input
+INPUT_MSG = ffg.gaussMessage(randn(1, DIM), randn(DIM, DIM), 'VARIANCE');
+%
 
 x.propagate(INPUT_MSG);
 
 RESULT_MSG = y.evidence();
 
-MEAN_EXPECTED = MATRIX * TestMultiplication.INPUT1.mean';
-VAR_EXPECTED = MATRIX * TestMultiplication.INPUT1.var * MATRIX';
+MEAN_EXPECTED = MATRIX * INPUT_MSG.mean';
+VAR_EXPECTED = MATRIX * INPUT_MSG.var * MATRIX';
 
 
 assertElementsAlmostEqual(RESULT_MSG.mean', MEAN_EXPECTED);
@@ -32,13 +33,13 @@ assertElementsAlmostEqual(RESULT_MSG.var, VAR_EXPECTED);
 
 function testBackward
 
-load('+ffg/+tests/data_gaussian');
+DIM = 3;
 
 nwk = ffg.Network;
 x = ffg.EvidenceNode;
 y = ffg.EvidenceNode;
 
-MATRIX = TestMultiplication.INPUT1.matrix;
+MATRIX = randn(DIM, DIM);
 A = ffg.MultiplicationNode;
 A.setMatrix(MATRIX);
 
@@ -46,17 +47,17 @@ nwk.addEdge(x, A);
 nwk.addEdge(A, y);
 
 % for now backward message only supports mean
-INPUT_MSG = struct('type', 'PRECISION', 'mean', TestMultiplication.INPUT1.mean, 'precision', TestMultiplication.INPUT1.var);
+INPUT_MSG = ffg.gaussMessage(randn(1, DIM), randn(DIM, DIM), 'VARIANCE'); 
 
 y.propagate(INPUT_MSG);
 
 RESULT_MSG = x.evidence();
 
-%VAR_EXPECTED = inv(MATRIX' * inv(TestMultiplication.INPUT1.var) * MATRIX);
-%MEAN_EXPECTED = VAR_EXPECTED * MATRIX' * inv(TestMultiplication.INPUT1.var) * TestMultiplication.INPUT1.mean';
+VAR_EXPECTED = inv(MATRIX' * inv(INPUT_MSG.var) * MATRIX);
+MEAN_EXPECTED = VAR_EXPECTED * MATRIX' * inv(INPUT_MSG.var) * INPUT_MSG.mean';
 
-%assertElementsAlmostEqual(RESULT_MSG.mean', MEAN_EXPECTED);
-%assertElementsAlmostEqual(RESULT_MSG.var, VAR_EXPECTED);
+assertElementsAlmostEqual(RESULT_MSG.mean', MEAN_EXPECTED);
+assertElementsAlmostEqual(RESULT_MSG.var, VAR_EXPECTED);
 
 
 
